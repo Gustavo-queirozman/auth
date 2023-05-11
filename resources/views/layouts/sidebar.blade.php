9cbafe9f216c07d8552d92ad8126e647afd5a3e0
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-05-11 17:02:23 -0300

    update

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body style="margin:0px; padding:0px;">
+        <header class="flex space-between" style="padding:15px;">
+            <img src="" alt="Logo">
+            <button>Sair</button>
+        </header>
+        <div style="display: flex;">
+            <nav style="background-color:red; height: 100vh; width:200px; diisplay: none;">
+                
+                    <ul>
+                        <li>Menu Principal</li>
+                        <li>xxx-xxxx-xxxxx</li>
+                    </ul>
+                    
+            
+
+            </nav>
+            <button>Menu</button>
+            <br>
+            <div class="content" style="display:flex; flex-wrap:wrap;">
+                <div>a</div>
+                <div>a</div>
+                <div>a</div>
+                <div>a</div>
+                <div>a</div>
+                <div>a</div>
+            </div>
+        </div>
+
+        <style>
+            .flex{
+                display: flex;
+            }
+            .space-between{
+                justify-content: space-between;
+            }
+        </style>
+    </body>
+</html>
