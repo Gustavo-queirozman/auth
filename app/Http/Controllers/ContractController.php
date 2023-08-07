86c63b5c944f22a2b43903a0ef094c7f0dacd2e6
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-08-06 21:45:33 -0300

    first commit

---
+++ app/Http/Controllers/ContractController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContractController extends Controller
+{
+    //mostra contrato
+    public function show(){
+        
+    }
+    //lista contratos
+    public function index(){
+        
+    }
+
+    //salva contrato
+    public function store(){
+
+    }
+    //edita contrato
+    public function edit(){
+
+    }
+    //atualiza contrato
+    public function update(){
+
+    }
+    //deleta contrato
+    public function delete(){
+
+    }
+
+}
